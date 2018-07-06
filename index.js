5a435f7096d049cb82643aa9af94009b5285a87d
Vijay Krishna Palepu <vpalepu@uci.edu>
2018-07-05 22:23:03 -0700

    Adding an FPS (frame-per-second) widget for monitoring UI perf

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
+
 THREE.TrackballControls = TrackballControls; //Link module to Three
 
 const graph3D = Graph3D();
